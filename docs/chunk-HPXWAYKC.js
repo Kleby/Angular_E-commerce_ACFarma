9009333d0200efce82f76d5566912ce1ce077756
Kleby <kleby10@gmail.com>
2024-05-14 23:39:52 -0300

    build do projeto para github pages

---
+++ docs/chunk-HPXWAYKC.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-YVB2MNMO.js";import"./chunk-EM3UTCME.js";export{a as ButtonActionComponent};
---
+++ docs/chunk-N66T66F4.js
@@ -0,0 +1,1 @@
+import{c as a}from"./chunk-F3F2QHTC.js";import"./chunk-EM3UTCME.js";export{a as ProductQuantityComponent};
